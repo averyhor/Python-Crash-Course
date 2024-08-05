94d830f8cc430e57d54da5b97b1cadac8bd420b6
Avery Hor <averyhor@Averys-MacBook-Air.local>
2024-08-05 21:48:51 +0800

    Chapter 2 Exercises

---
+++ Chapter 2 Exercises.py
@@ -0,0 +1,15 @@
+#2-3 Personal Message
+name = "avery"
+print(f"Hello {name.title()}, would you like to learn some Python today?")
+
+#2-4 Name Cases
+first = "avEry"
+middle = "cHin tEe"
+last = "hOr"
+
+full = f"{first} {middle} {last}"
+print(full.upper())
+print(full.lower())
+print(full.title())
+
+#2-5 Famous Quote
