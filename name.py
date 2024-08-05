04dee898cf0475d8b13e6615ced93c3db8df2f53
Avery Hor <averyhor@Averys-MacBook-Air.local>
2024-08-05 21:38:48 +0800

    Basic String Variable

---
+++ name.py
@@ -0,0 +1,14 @@
+name = "luna lovegood"
+print(name.title())
+
+print(name.upper())
+
+print(name.lower())
+
+first_name = "avery"
+last_name = "hor"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+nostarch_url = "https://nostarch.com"
+print(nostarch_url.removeprefix("https://"))
